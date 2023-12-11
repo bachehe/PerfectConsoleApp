4c3a1b5731887c70376f0041951c571a7783d64b
Damian Bachowski <bachowski.d@e80group.com>
2023-12-11 12:24:22 +0100

    addtests

--- UltimateLibraryTests/BusinessLogic/MessagesTest.cs
+++ UltimateLibraryTests/BusinessLogic/MessagesTest.cs
@@ -33,15 +33,26 @@ public class MessagesTest
             () => msgs.Greeting(key));
     }
     [Theory]
-    [InlineData("test", true)]
-    [InlineData(" ", true)]
-    [InlineData("-1!%51.31/5", true)]
-    public void IsValidString_ForInput_ShouldReturnTrue(string value, bool expected)
+    [InlineData("test")]
+    [InlineData(" ")]
+    [InlineData("-1!%51.31/5")]
+    public void IsValidString_ForInput_ShouldReturnTrue(string value)
     {
         ILogger<Messages> logger = new NullLogger<Messages>();
 
         Messages msgs = new(logger);
 
         Assert.True(msgs.IsValidString(value));
+    } 
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void IsValidString_ForInput_ShouldReturnFalse(string value)
+    {
+        ILogger<Messages> logger = new NullLogger<Messages>();
+
+        Messages msgs = new(logger);
+
+        Assert.False(msgs.IsValidString(value));
     }
 }
