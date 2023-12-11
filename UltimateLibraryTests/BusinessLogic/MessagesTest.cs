7b92e7c6917b521a00b871df7f4e714cd704484c
Damian Bachowski <bachowski.d@e80group.com>
2023-12-11 11:49:14 +0100

    feat tests

--- UltimateLibraryTests/BusinessLogic/MessagesTest.cs
+++ UltimateLibraryTests/BusinessLogic/MessagesTest.cs
@@ -1,11 +1,35 @@
-namespace UltimateLibraryTests.BusinessLogic
-{
-    public class MessagesTest
-    {
-        [Fact]
-        public void Test1()
-        {
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using UltimateLibrary.BusinessLogic;
 
-        }
+namespace UltimateLibraryTests.BusinessLogic;
+
+public class MessagesTest
+{
+    [Theory]
+    [InlineData("en", "Hello World")]
+    [InlineData("es", "Hola Mundo")]
+    public void Greetig_ForKey_ReturnCorrectTranslation(string key, string expected)
+    {
+        ILogger<Messages> logger = new NullLogger<Messages>();
+
+        Messages msgs = new(logger);
+
+        var result = msgs.Greeting(key);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Grretig_IncorrectKey_ReturnHandlingException()
+    {
+        var key = "idontexist";
+        var expected = "Sequence contains no elements";
+        ILogger<Messages> logger = new NullLogger<Messages>();
+
+        Messages msgs = new(logger);
+
+        Assert.Throws<Exception>(
+            () => msgs.Greeting(key));
     }
 }
